9bf5942e64066b65bfcc38a646371167de7f9955
Dennis Maltez <dennismaltez463@gmail.com>
2023-11-04 22:52:09 -0600

    problemas de css

--- AppHostServerJson/public/js/exa.js
+++ AppHostServerJson/public/js/exa.js
@@ -4,11 +4,9 @@ const contenedorPadre = document.getElementById('pushBoxFather');
 const buttonSend = document.getElementById('buttonSendExa');
 const infoPreguntas = document.getElementById('infoPreguntas');
 let numPreguntas = 0;
+// Eventos click
 buttonAñidirPregunta.addEventListener('click',funcionAñadirPreguntas)
-
-buttonBorrarPreguntas.addEventListener('click',()=>{
-funcionBorrarPreguntas()
-})
+buttonSend.addEventListener('click',fucionProcesarDatos)
 
 function funcionAñadirPreguntas(){
 numPreguntas ++
@@ -18,24 +16,43 @@ if(contenedorPadre.contains.length < 0){buttonSend.style.display = "none"}
 else buttonSend.style.display = "block";
 let div = document.createElement('DIV');div.classList.add('pushBox');
 let label = document.createElement('LABEL');label.setAttribute('contenteditable','true');
+let icon = document.createElement('IMG');icon.src = 'media/iconos/remove.png';icon.classList.add('iconForsRemoveQuestion')
 let input = document.createElement('INPUT');input.setAttribute('placeholder','Escriba aqui la pregunta que desea mandar');input.classList.add('inputsForExamenes');
-div.appendChild(label);div.appendChild(input);contenedorPadre.appendChild(div);
-}
-function funcionBorrarPreguntas(){
-try{
-if(numPreguntas == 0) statusNav.innerHTML = "No hay preguntas para borrar"
-else statusNav.innerHTML = "Selcione la pregunta que desea eliminar"
-let forInputs = document.querySelectorAll('.inputsForExamenes');
-forInputs.forEach(e=>{
-e.addEventListener('click',()=>{
-if(numPreguntas > 0) numPreguntas = numPreguntas - 1
- if(numPreguntas === 0){numPreguntas = 0;buttonSend.style.display = "none";infoPreguntas.style.display = "block"}
-statusNav.innerHTML = `Preguntas ${numPreguntas}`;
-contenedorPadre.removeChild(e.parentElement)
+div.appendChild(input);div.appendChild(icon);contenedorPadre.appendChild(div);
+	document.querySelectorAll('.iconForsRemoveQuestion').forEach(j=>{
+	j.addEventListener('click',()=>{	
+		console.clear()
+	 contenedorPadre.removeChild(j.parentElement)
+	 numPreguntas --
+	 if(numPreguntas === 0){ 
+	 	numPreguntas = 0
+	 	statusNav.innerHTML = `Preguntas ${numPreguntas}`;
+       statusNav.innerHTML = "Ingrese preguntas para enviarlas";
+       buttonSend.style.display = "none";
+       infoPreguntas.style.display = "block";
+	 }else {statusNav.innerHTML = `Preguntas ${numPreguntas}`;}
+	})
 })
-})
+
 }
-catch(e){
-infoPreguntas.innerHTML = "No hay preguntas para borrar"
+function fucionProcesarDatos(){
+let inputsQuestion = [];
+let pushData = {
+	numero : 0,
+	preguntas : []
 }
+let preguntasForInputs = document.querySelectorAll('.inputsForExamenes');
+preguntasForInputs.forEach(i=>{ 
+ inputsQuestion.push(i.parentElement.firstElementChild.value)
+});
+for(let i = 0; i < inputsQuestion.length; i++){
+	pushData.numero = inputsQuestion.length;
+	let rams = [ ... pushData.preguntas]
+    let copilador = rams.push(inputsQuestion[i])
+    pushData.preguntas = rams;
+}
+console.log(pushData)
+}
+function configByExa(){
+alert('calmaye es un codigo')
 }
